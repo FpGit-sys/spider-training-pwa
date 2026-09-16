216d56150fe2e6e7e458bb680eebac11fbb9cbd0
FpGit-sys <beyourgenius.ofc@gmail.com>
2026-09-16 20:04:03 -0300

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='spider-training-v3';
+const CORE=['./','./index.html','./styles.css','./exercises.js','./app.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return res;}).catch(()=>caches.match('./index.html'))));});
